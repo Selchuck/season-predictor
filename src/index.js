54d8f592210872350bd7c6e7bb8425dd3a4e70cb
Selchuk Karakus <selchuck.karakus@gmail.com>
2021-01-21 13:01:39 +0100

    implement conditional rendering and add componentDidMount lifecycle method for geo location

--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
-const App = () => {
-    return (
-        <div>
-            Hi!
-        </div>
-    )
-};
+class App extends React.Component {
+    state = {
+        latitude: null,
+        errorMessage: ''
+    };
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ latitude: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message }));
+    }
+
+    render() {
+        if (this.state.errorMessage && !this.state.latitude) {
+            return <div>Error Message: {this.state.errorMessage}</div>;
+        } if (!this.state.errorMessage && this.state.latitude) {
+            return <SeasonDisplay latitude={ this.state.latitude} />;
+        } else {
+            return <div>Loading...</div>;
+        }
+    }
+}
 
 ReactDOM.render(
     <App />, document.querySelector('#root')
